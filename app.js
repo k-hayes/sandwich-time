5879067f37da54395bc1a028aa42f4b50e612b5b
Jonathan Cokely <69878290+JonathanCokely@users.noreply.github.com>
2021-11-17 18:08:50 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let bread = ["white","whole wheat","rye","ciabatta","sourdough"];
+let protein = ["turkey","ham","roast beef","peanut butter","portabello", "tofu"];
+let cheese = ["american","cheddar","swiss","provolone","pepperjack","mozzerella"];
+let vegetables = [];
+let condiments = [];
+
