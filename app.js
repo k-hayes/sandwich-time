a8572c3eabb34666036b2a51ca3a2fae70b6e373
Jonathan Cokely <jon.cokely@gmail.com>
2021-11-24 19:34:30 -0600

    Generated ingredient lists

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
 let bread = ["white","whole wheat","rye","ciabatta","sourdough"];
 let protein = ["turkey","ham","roast beef","peanut butter","portabello", "tofu"];
 let cheese = ["american","cheddar","swiss","provolone","pepperjack","mozzerella"];
-let vegetables = ["lettuce","tomato","onion","pickles","banana peppers"];
-let condiments = ["mustard","mayo","kimchi","ranch"];
+let vegetable = ["lettuce","tomato","onion","pickles","banana peppers"];
+let condiment = ["mustard","mayo","kimchi","ranch","chili crunch"];
 
 window.addEventListener("load", function(){
     const btn = document.getElementById("btn");
     const sandwich = document.getElementById("sandwich");
     const breadChild = document.getElementById("bread-child");
-
-    // function IngredientCounter(ingredient){
-    //     for(let i=0; i>count(ingredient)-1; i++){
-    //         document.createElement("<div>'Ingredient'</div>")
-    //     }
-    // };
+    const proteinChild = document.getElementById("protein-child");
+    const cheeseChild = document.getElementById("cheese-child");
+    const vegetablesChild = document.getElementById("vegetables-child");
+    const condimentsChild = document.getElementById("condiments-child");
 
     let breads = "";
-        for(let i=0; i>(bread.length-1); i++){
-            console.log(bread[i]);
-            // let newLine = document.createElement("<div>'Ingredient'</div>")
-            breads = breads + "\n Ingredient";
-        }
+    for(let i=0; i<(bread.length); i++){
+       breads = breads + `<div>${bread[i]}</div>`
+    }
+    breadChild.innerHTML = `<div>${breads}</div>`;
 
-    breadChild.innerHTML = breads;
+    let proteins = "";
+    for(let i=0; i<(protein.length); i++){
+       proteins = proteins + `<div>${protein[i]}</div>`
+    }
+    proteinChild.innerHTML = `<div>${proteins}</div>`;
+
+    let cheeses = "";
+    for(let i=0; i<(cheese.length); i++){
+       cheeses = cheeses + `<div>${cheese[i]}</div>`
+    }
+    cheeseChild.innerHTML = `<div>${cheeses}</div>`;
+
+    let vegetables = "";
+    for(let i=0; i<(vegetable.length); i++){
+       vegetables = vegetables + `<div>${vegetable[i]}</div>`
+    }
+    console.log(vegetables);
+    vegetablesChild.innerHTML = `<div>${vegetables}</div>`;
+    
+
+    let condiments = "";
+    for(let i=0; i<(condiment.length); i++){
+       condiments = condiments + `<div>${condiment[i]}</div>`
+    }
+    condimentsChild.innerHTML = `<div>${condiments}</div>`;
     
 
     btn.addEventListener('click', function(){   
-        sandwich.innerHTML = `${protein[1]}, ${cheese[2]}, ${vegetables[1]}, ${condiments[1]} on ${bread[0]}`;
+        sandwich.innerHTML = `${protein[1]}, ${cheese[2]}, ${vegetable[1]}, ${condiment[1]} on ${bread[0]}`;
     });
 })
 
